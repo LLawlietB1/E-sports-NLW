bff40924afaa56c70b41f3029ab1302b6c81a01a
LLawlietB1 <LucasLoaRafa@gmail.com>
2025-07-09 16:04:55 -0300

    function sendForm add

--- script.js
+++ script.js
@@ -6,8 +6,34 @@ const aiResponse = document.getElementById('aiResponse')
 const form = document.getElementById('form')
 
 
+const questionAI = async () => {
+    
+}
+
 const sendForm = (event) => {
     event.preventDefault()
-    console.log(event)
+    const apiKey = apiKeyInput.value 
+    const game = gameSelect.value 
+    const question = questionInput.value
+
+    console.log({apiKey, game, question})
+
+    if(apiKey == '' || game == '' || question == '') {
+        alert('Por favor, preencha todos os campos')
+        return
+    }
+    askButton.disabled = true
+    askButton.textContent = "Perguntando..."
+    askButton.classList.add('loading')
+
+    try{ 
+        //Perguntar para a IA
+    } catch(error) {
+        console.log('Erro: ', error)
+    } finally {
+        askButton.disabled = false;
+        askButton.textContent = "Perguntar"
+        askButton.classList.remove("loading")
+    }
 }
 form.addEventListener('submit', sendForm)
